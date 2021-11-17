bda55e9851829a2520e5e6c8e8a6eea76b67634f
Opilane TTHK <opilane@tthk.ee>
2021-11-17 12:37:01 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+namespace DemoApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderName = @"C:\Users\opilane\samples\TITpe21";
+            if (!Directory.Exists(folderName))
+            {
+                Directory.CreateDirectory(folderName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RandomQuote/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Quotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(GetRandomQuote());
+
+        }
+
+        public static string GetRandomQuote()
+        {
+            string filepath = @"C:\Users\opilane\samples\TITpe21\Quotes.txt";
+            string[] Quotes = File.ReadAllLines(filepath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, Quotes.Length);
+
+
+
+            return Quotes[randomIndex];
+        }
+    }
+}
